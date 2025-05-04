c343b99f90b6f67c49b920b47666984bf1029391
Ilnur <ilnuraglyamov@yandex.ru>
2025-05-05 01:02:36 +0300

    Card info

--- frontend/src/CardItem.jsx
+++ frontend/src/CardItem.jsx
@@ -1,8 +1,6 @@
-import { Icon, Box, Text, Flex, Image, Link } from "@chakra-ui/react"
-import { SlClock } from "react-icons/sl";
-import { CiStar } from "react-icons/ci";
-import { FaStar } from "react-icons/fa6";
+import { Icon, Box, Text, Flex, Image, Link, NumberInput } from "@chakra-ui/react"
 import ImageCard from './assets/1.jpg'
+import { HiMiniShoppingCart } from "react-icons/hi2";
 
 export default function CardItem() {
   return (
@@ -10,8 +8,18 @@ export default function CardItem() {
     <Image src={ImageCard} alt="test" rounded="8px 8px 0 0" />
     <Flex p='30px 40px 30px 20px' direction='column' gap='10px'>
       <Link fontSize="24px" fontWeight="bold">Test</Link>
-      <Flex justify={"space-between"} align={'center'}>
-        Test
+      <Flex justify={"space-between"} align={'center'} direction={{ base: 'column', md: 'row' }}>
+        <Text>Цена: 100 руб</Text>
+        <NumberInput.Root min="0" width="100px">
+          <NumberInput.Label />
+          <NumberInput.Control>
+            <NumberInput.IncrementTrigger />
+            <NumberInput.DecrementTrigger />
+          </NumberInput.Control>
+          <NumberInput.Scrubber />
+          <NumberInput.Input />
+        </NumberInput.Root>
+        <HiMiniShoppingCart size="48px" fontWeight="bold"/>
       </Flex>
     </Flex>
     </Box>
