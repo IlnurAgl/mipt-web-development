84bacb395f72d6f2f6afe0f578b87b0f99cac050
Ilnur <ilnuraglyamov@yandex.ru>
2025-05-09 04:03:20 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react({
     jsxRuntime: 'classic' // Add this line
-  }), viteTsconfigPaths(), svgrPlugin()],
-  // resolve: {
-  //   alias: {
-  //     '@/': `${path.resolve(__dirname, 'src')}/`,
-  //   },
-  // },
+  }), ],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   // server: {
   //   fs: {
   //     cachedChecks: false
