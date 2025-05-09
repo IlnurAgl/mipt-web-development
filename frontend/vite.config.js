8c7ab98445f920e33a7af69f97eaa3f34616c160
Ilnur <ilnuraglyamov@yandex.ru>
2025-05-09 03:56:14 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,14 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
+  // resolve: {
+  //   alias: {
+  //     '@/': `${path.resolve(__dirname, 'src')}/`,
+  //   },
+  // },
+  server: {
+    fs: {
+      cachedChecks: false
+    }
   }
 })
